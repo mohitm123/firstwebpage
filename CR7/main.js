3ee29238215487790f23458922c2124669d7e61a
mohitm123 <35539313+mohitm123@users.noreply.github.com>
2019-01-02 16:04:47 +0530

    Add files via upload

---
+++ CR7/main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.innerHTML === "Credits") {
+    x.innerHTML = "Credits";
+  } else {
+    x.innerHTML = "mmaroliya!";
+  }
+}
